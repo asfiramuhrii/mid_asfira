22bb6a562bb089b19df3e69c3ac6dfae3c148fac
LAPTOP-5OM1FA72\Asus <60900119024@uin-alauddin.ac.id>
2021-11-09 11:42:24 +0800

    soal nomor 6

---
+++ resources/views/mid.blade.php
@@ -0,0 +1,25 @@
+<!-- Version control adalah sebuah sistem yang mencatat setiap perubahan terhadap sebuah berkas atau kumpulan berkas sehingga pada suatu saat anda dapat kembali kepada salah satu versi dari berkas tersebut. 
+Sebagai contoh dalam buku ini anda akan menggunakan kode sumber perangkat lunak sebagai berkas yang akan dilakukan version controlling, meskipun pada kenyataannya anda dapat melakukan ini pada hampir semua tipe berkas di komputer. 
+version control system (VCS) adalah sebuah kumpulan perangkat lunak yang sudah terintegrasi dan digunakan untuk membantu software engineer mengelola perubahan dalam source code dari waktu ke waktu. 
+Software dalam sistem ini mampu melacak setiap modifikasi kode dalam seluruh jenis database perusahaan. 
+Jika software menemukan kesalahan, engineer dapat membandingkan kode dari versi sebelumnya dan mulai memperbaiki kekeliruan tersebut.
+Karena sistem pelacakan yang akurat, version control system dapat mengurangi gangguan serta kesalahan pada kinerja semua anggota tim engineering.
+Dalam hampir semua software project, source code adalah sebuah aset berharga yang harus dilindungi.  -->
+
+<!-- 5 contoh
+1. Bazaar merupakan bagian dari GNU Project, Bazaar adalah free software yang disponsori oleh Canonical. 
+Salah satu layanan yang menggunakan Bazaar adalah Launchpad, sebuah tempat dimana aplikasi Ubuntu dikembangkan dan dipantau oleh komunitas. 
+Bazaar dapat digunakan di Windows, Ubuntu, Debian, Red Hat, SUSE, OS X, FreeBSD, Solaris, Gentoo, dan lainnya.
+
+2. SVN adalah free VCS yang didesain mirip dengan CVS dan lebih sederhana. 
+SVN mendukung atomic commits dari sebuah file dan melakukan versioningterhadap direktori, symbolic links, dan meta-data. 
+
+3. Mercurial merupakan version control system terdistribusi berbasis open source. 
+Mercurial dirancang untuk proyek yang lebih besar, kemungkinan besar berada di luar lingkup designer dan independen Web Developer.
+
+4. CVS adalah revision control systems yang pertama kali dibuat. pertama kali dirilis pada tahun 1986, dan Google Code masih menjadi tuan rumah posting Usenet yang asli yang mengumumkan CVS. 
+CVS adalah standar de facto dan dipasang hampir di mana-mana. Namun, basis kode tidak sepenuhnya ditampilkan sebagai SVN atau solusi lainnya.
+
+5. Revision Control System (RCS) mampu mengelola revisi dari banyak file. 
+RCS mengotomasi penyimpanan, pengambilan, pencatatan, pemeriksaan, dan penggabungan dari sebuah revisi. 
+Berguna untuk teks yang sering direvisi seperti source code, program, dokumentasi, grafik, jurnal, dan surat. -->
