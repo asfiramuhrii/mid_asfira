3fc33dce3eb8167ad6fae0452e79aa3742bf1962
LAPTOP-5OM1FA72\Asus <60900119024@uin-alauddin.ac.id>
2021-11-09 11:13:39 +0800

    salo sawae, 23 september 20021

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('mid kakas asfira muhri kelas b nim 60900119024, semangat bestieeeeee');
 });
